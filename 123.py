564a3c8a4f75ddad354435668f613d711ab041bc
Matreshka21 <21.11.1989@list.ru>
2023-07-06 21:07:24 +0300

    Added all

---
+++ 123.py
@@ -0,0 +1,10 @@
+def even_or_odd(a):
+    if a % 2 == 0:
+        print('Четное число')
+    else:
+        print('Нечентное число')
+
+even_or_odd(10)
+>>> Четное число
+even_or_odd(11)
+>>> Нечетное число
---
+++ 2121.py
@@ -0,0 +1,4 @@
+nambers = [12, 15, 19, 22, 33]
+for numbers in nambers:
+    print(number)
+    if number % 2 == 0
